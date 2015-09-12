e3990b2b4d6538358c185d6f4cfe6df9ea8874c1
XJi <xiaojingji5@gmail.com>
2015-09-12 19:47:12 -0400

    SignIn/SignUp

--- ShowMe/app/src/main/java/com/qr/android/showme/MainActivity.java
+++ ShowMe/app/src/main/java/com/qr/android/showme/MainActivity.java
@@ -1,5 +1,6 @@
 package com.qr.android.showme;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -8,14 +9,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import com.parse.Parse;
-import com.parse.ParseObject;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button openQRScannerButton = (Button) findViewById(R.id.openQRScannerButton);
         openQRScannerButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,10 +36,15 @@ public class MainActivity extends ActionBarActivity {
 
             }
         });
-        // Enable Local Datastore.
-        Parse.enableLocalDatastore(this);
 
-        Parse.initialize(this, "yCdbM12FKZAqcHEvp4PIEFVDEZHMhJNkSUKuxc0d", "M75faIfv4O3DSmhK6gqnI0mBHIHBxNIRDJseHNA5");
+        Button signUpButton = (Button) findViewById(R.id.signupbutton);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SignInActivity.class));
+            }
+        });
+
 
     }
 
---
+++ ShowMe/app/src/main/java/com/qr/android/showme/data.java
@@ -0,0 +1,8 @@
+package com.qr.android.showme;
+
+/**
+ * Created by Ji on 9/12/15.
+ */
+public  class data {
+    public static boolean ret = false;
+}
