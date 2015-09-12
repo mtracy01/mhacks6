9ade95a5af72ead84982f34597c0b82cc573bd79
sh953 <mannandd@gmail.com>
2015-09-12 02:35:13 -0400

    QRScannerActivity opens

--- ShowMe/app/src/main/java/com/qr/android/showme/MainActivity.java
+++ ShowMe/app/src/main/java/com/qr/android/showme/MainActivity.java
@@ -1,9 +1,12 @@
 package com.qr.android.showme;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,15 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button openQRScannerButton = (Button) findViewById(R.id.openQRScannerButton);
+        openQRScannerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,QRScannerActivity.class);
+                MainActivity.this.startActivity(intent);
+
+            }
+        });
     }
 
 
